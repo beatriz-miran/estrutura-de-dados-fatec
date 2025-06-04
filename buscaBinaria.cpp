de990b22ef0da9b6edd8d209ddb0ed96e3fe2690
Beatriz Miranda <canaisdarkbia@gmail.com>
2025-06-04 11:34:59 -0300

    Create buscaBinaria.cpp

---
+++ buscaBinaria.cpp
@@ -0,0 +1,57 @@
+/*
+	Name: Buscabinaria.cpp
+	Author: Beatriz Miranda da Silva
+	Date: 04/06/25 10:54
+	Description: implementaçaodo metodo de recuperação de informação chamado busca binaria ou logaritimica
+*/
+
+#include <stdio.h>
+
+ main(){
+ 	
+	//int vet[] = { 93, 72, 54, 45, 35, 28, 21, 17, 12, 3};	//Pior caso
+	int vet[] = { 3, 12, 17, 21, 28, 35, 45, 54,72, 93};	//Melhor caso
+ 	
+ 	int i, inicio, meio, fim;
+ 	int seek, comp = 0; //quem voce vai localizar "procurar"
+ 	int flag = 0; //valor simulado booleano
+ 	
+ 	int tam = sizeof(vet)/sizeof(int);	
+ 	
+ 	puts("Conteudo do vetor:");
+	for(i = 0; i < tam; i++){
+		printf("%d | ", vet[i]);
+	};
+ 	
+ 	printf("\nDigite o valor a ser pesquisado: ");
+ 	scanf("%d", &seek);
+ 	
+ 	inicio = 0;
+ 	fim = tam -1;
+ 	
+
+ 	while(flag != 1 && inicio <= fim){
+ 		meio = (inicio + fim) / 2;
+ 		
+ 		
+ 		if(vet[meio] == seek)
+ 			flag = 1;
+ 		if(vet[meio] > seek)
+ 			fim = meio - 1;
+ 		else
+ 			inicio = meio + 1;
+		 
+	comp++;
+	 }
+	 
+
+	 if(flag == 1){
+	 	printf("O elemento %d buscado esta na %da posicao. A quantidade de comparacoes eh: %d", vet[meio], meio+1, comp);
+	 }else{
+	 	puts("Elemento nao encontrado");
+	 }
+	 
+	
+ 		
+ 	
+ }//fim do programa
